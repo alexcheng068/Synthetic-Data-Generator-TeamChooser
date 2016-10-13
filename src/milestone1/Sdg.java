76daf5b04d7a1abd5e65a20c8e50b92878feba09
Alex Cheng <Alex Cheng>
2016-10-13 03:45:18 -0400

    added off and def

--- src/milestone1/Sdg.java
+++ src/milestone1/Sdg.java
@@ -14,7 +14,7 @@ public class Sdg {
 
 	private  void ShuffleArray(String[][] array)
 	{
-		String  temp0, temp1;
+		String  temp0, temp1,temp2;
 		int index;
 		Random random = new Random();
 		for (int i = array.length - 1; i > 0; i--)
@@ -22,10 +22,13 @@ public class Sdg {
 			index = random.nextInt(i + 1);
 			temp0 = array[index][0];
 			temp1 = array[index][1];
+			temp2 = array[index][2];
 			array[index][0] = array[i][0];
-			array[index][1] = array[i][1];       
+			array[index][1] = array[i][1];
+			array[index][2] = array[i][2];   
 			array[i][0] = temp0;
 			array[i][1] =temp1;
+			array[i][2] =temp2;
 		}
 	}
 
@@ -46,26 +49,28 @@ public class Sdg {
 			int count=0;
 			while ((dummyline = brcount.readLine()) != null) {count++;}
 
-			String[][] name_rating= new String[count][2];
+			String[][] name_rating= new String[count][3];
 			int i=0;
 			while ((line = br.readLine()) != null){		
 				// use comma as separator
 				String[] tmp = line.split(cvsSplitBy);
 				name_rating[i][0]=tmp[1];
 				name_rating[i][1]=tmp[3];
+				name_rating[i][2]=tmp[5];
 				//System.out.println( name_rating[i][1]+"         "+ name_rating[i][0]);
 				i++;
 			}
 
-			String[][] name_rating_tmp= new String[count-1][2];
+			String[][] name_rating_tmp= new String[count-1][3];
 			for(i=1;i<count;i++){
 				name_rating_tmp[i-1][0]=name_rating[i][0];
 				name_rating_tmp[i-1][1]=name_rating[i][1];
+				name_rating_tmp[i-1][2]=name_rating[i][2];
 				//System.out.println(name_rating_tmp[i-1][0]+"  "+name_rating_tmp[i-1][1]);
 			}
 
-			int [] score_count= new int [10];
-			for (i=0;i<10;i++){//initualize  
+			int [] score_count= new int [15];
+			for (i=0;i<15;i++){//initualize  
 				score_count[i]=0;
 			}
 
@@ -92,7 +97,7 @@ public class Sdg {
 				//Min + (int)(Math.random() * ((Max - Min) + 1))
 				players_per_team=rand.nextInt((max_players_per_team-min_players_per_team)+1)+min_players_per_team;
 				System.out.println("player per team"+players_per_team);
-				String[][] game= new String [players_per_team+1][4];//6 player per team, 7th row is score
+				String[][] game= new String [players_per_team+1][6];//6 player per team, 7th row is score
 				//decide whether odd numbers of players
 				//probability is 0.25 
 				boolean odd=false;
@@ -106,26 +111,29 @@ public class Sdg {
 				System.out.println("odd is "+odd);
 				for (int p_count=0;p_count<players_per_team;p_count++){
 					game[p_count][0]=name_rating_tmp[p_count][0];
-					game[p_count][1]=name_rating_tmp[p_count][1];
+					game[p_count][1]=name_rating_tmp[p_count][2];
+					game[p_count][2]=name_rating_tmp[p_count][1];
 
 					if(p_count==players_per_team-1 && odd==true){
-						game[p_count][2]="";
 						game[p_count][3]="";
+						game[p_count][4]="";
+						game[p_count][5]="";
 					}
 					else{
-						game[p_count][2]=name_rating_tmp[p_count+players_per_team][0];
-						game[p_count][3]=name_rating_tmp[p_count+players_per_team][1];		
-						sumB=sumB+ Double.parseDouble(game[p_count][3]);
+						game[p_count][3]=name_rating_tmp[p_count+players_per_team][0];
+						game[p_count][4]=name_rating_tmp[p_count+players_per_team][2];
+						game[p_count][5]=name_rating_tmp[p_count+players_per_team][1];	
+						sumB=sumB+ Double.parseDouble(game[p_count][5]);
 					}
-					sumA=sumA+ Double.parseDouble(game[p_count][1]);
+					sumA=sumA+ Double.parseDouble(game[p_count][2]);
 
 				}
 				double avgA=sumA/players_per_team;
 				double avgB=sumB/players_per_team;
 				System.out.println("avg a is "+avgA+" avg b is "+avgB);
 				if (avgA==avgB){
-					game[players_per_team][3]="0";
-					game[players_per_team][1]="0";
+					game[players_per_team][5]="0";
+					game[players_per_team][2]="0";
 					score_count[0]=score_count[0]+1;
 
 				}
@@ -133,8 +141,8 @@ public class Sdg {
 					double diff=(avgA-avgB)*2;
 					Long L = Math.round(diff);
 					int score = Integer.valueOf(L.intValue());				
-					game[players_per_team][1]=String.valueOf(score);
-					game[players_per_team][3]="0";
+					game[players_per_team][2]=String.valueOf(score);
+					game[players_per_team][5]="0";
 					totalscore+=score;
 					score_count[score]=score_count[score]+1;
 					System.out.println("score scount is "+score_count[score]);
@@ -143,8 +151,8 @@ public class Sdg {
 					double diff=(avgB-avgA)*2;
 					Long L = Math.round(diff);
 					int score = Integer.valueOf(L.intValue());				
-					game[players_per_team][3]=String.valueOf(score);
-					game[players_per_team][1]="0";
+					game[players_per_team][5]=String.valueOf(score);
+					game[players_per_team][2]="0";
 					totalscore+=score;
 					score_count[score]=score_count[score]+1;
 				}
@@ -156,10 +164,21 @@ public class Sdg {
 				String gameid= "Game ID: ,"+String.valueOf(k+1);
 				String [] gameidString = gameid.split(",");
 				writer.writeNext(gameidString);
-
+				String [] gameTitle = new String[6];
+				gameTitle[0]="player id";
+				gameTitle[1]="position";
+				gameTitle[2]="Rating";
+				gameTitle[3]="player id";
+				gameTitle[4]="position";
+				gameTitle[5]="Rating";
+				writer.writeNext(gameTitle);
 				for ( int m=0; m < players_per_team+1; m++) {
-					String [] gameString = new String[4];
-					for (int j=0;j<4;j++){
+					
+
+					
+					
+					String [] gameString = new String[6];
+					for (int j=0;j<6;j++){
 						gameString[j]=game[m][j];
 					}
 					writer.writeNext(gameString);
@@ -195,7 +214,7 @@ public class Sdg {
 			System.out.println("totoal score: "+totalscore);
 			System.out.println("avgscore: "+avgscore);
 			System.out.println("score distribution is :");
-			for (i=0;i<10;i++){
+			for (i=0;i<15;i++){
 				double score_dis= (double)score_count[i]*100/(double)num_of_game;
 				System.out.println("score diff "+i+" is "+score_dis +"%");
 			}
